8b70e22b0168059c0872a7c3cd5bfccb26df841f
AMINONA <114190409+AMINONA@users.github.noreply.com>
2026-06-09 10:46:55 +0200

    fixed number of values returned by get_accounts

--- projet.py
+++ projet.py
@@ -23,7 +23,7 @@ def get_accounts():
     try:
         with open("accounts.json", "r") as file:
             accounts = json.load(file)
-        return("succes",accounts)
+        return(accounts)
     except (FileNotFoundError, json.JSONDecodeError):
         accounts = {}
         return(accounts)
