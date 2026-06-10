9e936c707cd7c9dcc1e8eea2cab517bd1ecca7cf
AMINONA <114190409+AMINONA@users.noreply.github.com>
2026-06-10 22:55:03 +0200

    added build .bat to build the app

---
+++ build.bat
@@ -0,0 +1,3 @@
+py -m PyInstaller --onefile --windowed --name "VaultKey" --add-data "index.html;." projet.py
+REM py -m PyInstaller --onefile --windowed --icon=icon.ico --add-data "index.html;." projet.py
+pause
