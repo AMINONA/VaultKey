fa99537d0a77f40479df01d8d2fcaf6768bd39e3
AMINONA <114190409+AMINONA@users.noreply.github.com>
2026-06-11 15:08:48 +0200

    - added icon - updated .bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-py -m PyInstaller --onefile --windowed --name "VaultKey" --add-data "index.html;." projet.py
+py -m PyInstaller --onefile --windowed --icon=icon.ico --name "VaultKey" --add-data "index.html;." projet.py
 REM py -m PyInstaller --onefile --windowed --icon=icon.ico --add-data "index.html;." projet.py
 pause
