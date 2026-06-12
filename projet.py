dfdfc8f2d38f2851d240815f4f3a705f9ec75256
AMINONA <114190409+AMINONA@users.github.noreply.com>
2026-06-12 07:30:02 +0200

    test4

--- projet.py
+++ projet.py
@@ -222,7 +222,7 @@ def update_app():
         REM launch VaultKey.exe
         timeout /t 5 /nobreak >nul
 
-        start "" "{APP_DIR}\VaultKey.exe"
+        explorer.exe "%APP_DIR%\VaultKey.exe"
 
         del "%~f0"
     """
